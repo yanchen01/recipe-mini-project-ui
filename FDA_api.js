bba5853807d261dc28ef07df516ee7dec96ce103
Hanlin Mai <hanlinm@bu.edu>
2021-09-15 11:31:34 -0400

    Adding api 2

---
+++ FDA_api.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const transfer_barcode = ({ type, data }) => {
+    alert(`Bar code with type ${type} and data ${data} has been scanned.`);
+    console.log(url_head + data);
+}
+
+const api_work = () => {
+
+}
